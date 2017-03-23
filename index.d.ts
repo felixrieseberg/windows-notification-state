48b8ddac70bd2f4bc68960bc6775ec46c7cb6d4e
Felix Rieseberg <felix@felixrieseberg.com>
2017-03-23 12:49:00 -0700

    :memo: Better types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 declare module 'windows-notification-state' {
-  export function getNotificationState(): string;
+  export function getNotificationState(): QUERY_USER_NOTIFICATION_STATE | 'UNKNOWN_ERROR';
   export function shQueryUserNotificationState(): number;
+  export type QUERY_USER_NOTIFICATION_STATE = 'QUNS_NOT_PRESENT' | 'QUNS_BUSY' | 'QUNS_RUNNING_D3D_FULL_SCREEN' | 'QUNS_PRESENTATION_MODE' | 'QUNS_ACCEPTS_NOTIFICATIONS' | 'QUNS_QUIET_TIME' | 'QUNS_APP';
 }
