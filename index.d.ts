84b22c37820f0e6d21901a1faac1185b4086c8fb
Yagiz Nizipli <yagiz@nizipli.com>
2022-06-21 09:51:24 -0400

    fix: update typings module prefix

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'windows-notification-state' {
+declare module '@stack-inc/windows-notification-state' {
   export function getNotificationState(): QUERY_USER_NOTIFICATION_STATE | 'UNKNOWN_ERROR';
   export function shQueryUserNotificationState(): number;
   export type QUERY_USER_NOTIFICATION_STATE = 'QUNS_NOT_PRESENT' | 'QUNS_BUSY' | 'QUNS_RUNNING_D3D_FULL_SCREEN' | 'QUNS_PRESENTATION_MODE' | 'QUNS_ACCEPTS_NOTIFICATIONS' | 'QUNS_QUIET_TIME' | 'QUNS_APP';
